e6044800b7607d5202ea258382bdec5eff81f76d
Russell Howell <rhowell1110@gmail.com>
2019-06-19 19:35:29 -0400

    Inital commit

---
+++ src/components/LoginLanding/LoginLanding.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface ILoginLandingProps {
+}
+
+const LoginLanding: React.FunctionComponent<ILoginLandingProps> = (props) => {
+  return(
+      <div/>
+  );
+};
+
+export default LoginLanding;
---
+++ src/components/TasksDashboard/TasksDashboard.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface ITasksDashboardProps {
+}
+
+const TasksDashboard: React.FunctionComponent<ITasksDashboardProps> = (props) => {
+  return( 
+      <div/>
+  ) ;
+};
+
+export default TasksDashboard;
